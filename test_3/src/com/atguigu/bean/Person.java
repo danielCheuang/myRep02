709f6d40fd7e1d886cf468a2018dc8697fdb8026
danielCheuang <daniel2020@sina.com>
2017-02-19 16:17:45 +0800

    test_3

---
+++ test_3/src/com/atguigu/bean/Person.java
@@ -0,0 +1,39 @@
+package com.atguigu.bean;
+
+public class Person {
+	private String id;
+	private String name;
+
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Person(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
+	}
+
+}
